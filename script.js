a109ba227245e23404b6a1b137290bb8c612d6b9
Jenny Ryden <jennyryden@protonmail.com>
2021-01-15 16:27:00 +0100

    Set progress bar

--- script.js
+++ script.js
@@ -119,7 +119,16 @@ function updateProgressBar(e) {
   }
 }
 
+// Set progress bar
+function setProgressBar(e) {
+  const width = this.clientWidth
+  const clickX = e.offsetX
+  const { duration } = music
+  music.currentTime = (clickX / width) * duration
+}
 // Event Listeners
 prevBtn.addEventListener('click', prevSong)
 nextBtn.addEventListener('click', nextSong)
+music.addEventListener('ended', nextSong)
 music.addEventListener('timeupdate', updateProgressBar)
+progressContainer.addEventListener('click', setProgressBar)
